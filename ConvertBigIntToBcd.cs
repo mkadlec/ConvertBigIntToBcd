0faf85f7b9af4162947b836a06f66036613ddd7a
Mark Kadlec <kadlecmark@hotmail.com>
2013-01-03 21:23:22 -0800

    Create ConvertBigIntToBcd.cs

---
+++ ConvertBigIntToBcd.cs
@@ -0,0 +1,30 @@
+        public static byte[] ConvertBigIntToBcd(long numberToConvert, int howManyBytes, byte paddingChar)
+        {
+            var convertedNumber = new byte[howManyBytes];
+            var strNumber = numberToConvert.ToString();
+            var currentNumber = string.Empty;
+
+            for (var i = 0; i < howManyBytes; i++)
+            {
+                convertedNumber[i] = 0xff;
+            }
+
+            for (var i = 0; i < strNumber.Length; i++)
+            {
+                currentNumber += strNumber[i];
+
+                if (i == strNumber.Length - 1 && i % 2 == 0)
+                {
+                    convertedNumber[i / 2] = 0xf;
+                    convertedNumber[i / 2] |= (byte)((int.Parse(currentNumber) % 10) << 4);
+                }
+
+                if (i % 2 == 0) continue;
+                var value = int.Parse(currentNumber);
+                convertedNumber[(i - 1) / 2] = (byte) (value % 10);
+                convertedNumber[(i - 1) / 2] |= (byte)((value / 10) << 4);
+                currentNumber = string.Empty;
+            }
+
+            return convertedNumber;
+        }
